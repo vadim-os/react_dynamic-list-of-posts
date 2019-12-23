498a1df2b8c55369c1c61e7bba7a0d8def764bae
vadim-os <osinsky_vadim@yahoo.com>
2019-12-23 14:34:35 +0200

    Updates

---
+++ precache-manifest.482d3892bbcb5ab04a954a5867b29d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d21b8de8ac276ed46833cfe62eef7c9",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "54d24b1592b5e28786d4",
+    "url": "/react_dynamic-list-of-posts/static/css/main.0791751e.chunk.css"
+  },
+  {
+    "revision": "a93dcb5f4871ee723658",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a2bac020.chunk.js"
+  },
+  {
+    "revision": "54d24b1592b5e28786d4",
+    "url": "/react_dynamic-list-of-posts/static/js/main.f80f7172.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
